cbcd7404ce4bb2fb150c80f0f98a9a14b96eb630
sfla <stiian@mac.com>
2024-11-18 23:07:22 +0100

    Added some logs

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
 let Service, Characteristic;
 
-class ToshibaHomeACControlAccessory {
-  constructor(log, config, api) {
-    this.log = log;
-    this.name = config.name || 'Toshiba AC';
-    this.config = config;
-
-    // Get the Service and Characteristic from the Homebridge API
-    Service = api.hap.Service;
-    Characteristic = api.hap.Characteristic;
-
-    // Setup the Thermostat service
-    this.service = new Service.Thermostat(this.name);
-
-    // Bind characteristics to their handlers
-    this.service
-      .getCharacteristic(Characteristic.CurrentTemperature)
-      .on('get', this.handleCurrentTemperatureGet.bind(this));
-
-    this.service
-      .getCharacteristic(Characteristic.TargetTemperature)
-      .on('set', this.handleTargetTemperatureSet.bind(this));
-  }
-
-  handleCurrentTemperatureGet(callback) {
-    // Placeholder: Replace with actual logic to get temperature
-    callback(null, 22); // Example: return 22 degrees
-  }
-
-  handleTargetTemperatureSet(value, callback) {
-    // Placeholder: Replace with actual logic to set temperature
-    this.log(`Set target temperature to: ${value}`);
-    callback();
-  }
-
-  getServices() {
-    return [this.service];
-  }
-}
-
 module.exports = (api) => {
-  api.registerAccessory('ToshibaHomeACControlAccessory', ToshibaHomeACControlAccessory);
+    console.log('Registering ToshibaHomeACControlAccessory...');
+    api.registerAccessory('ToshibaHomeACControlAccessory', ToshibaHomeACControlAccessory);
 };
+
+class ToshibaHomeACControlAccessory {
+    constructor(log, config, api) {
+        console.log('Initializing ToshibaHomeACControlAccessory...'); // Debug log
+        log('Constructor called for ToshibaHomeACControlAccessory'); // Homebridge log
+        this.log = log;
+        this.name = config.name || 'Toshiba AC';
+        this.config = config;
+        
+        log(`Accessory Name: ${this.name}`);
+        
+        // Get the Service and Characteristic from the Homebridge API
+        Service = api.hap.Service;
+        Characteristic = api.hap.Characteristic;
+        
+        // Setup the Thermostat service
+        this.service = new Service.Thermostat(this.name);
+        
+        // Bind characteristics to their handlers
+        this.service
+        .getCharacteristic(Characteristic.CurrentTemperature)
+        .on('get', this.handleCurrentTemperatureGet.bind(this));
+        
+        this.service
+        .getCharacteristic(Characteristic.TargetTemperature)
+        .on('set', this.handleTargetTemperatureSet.bind(this));
+    }
+    
+    handleCurrentTemperatureGet(callback) {
+        // Placeholder: Replace with actual logic to get temperature
+        callback(null, 22); // Example: return 22 degrees
+    }
+    
+    handleTargetTemperatureSet(value, callback) {
+        // Placeholder: Replace with actual logic to set temperature
+        this.log(`Set target temperature to: ${value}`);
+        callback();
+    }
+    
+    getServices() {
+        return [this.service];
+    }
+}
